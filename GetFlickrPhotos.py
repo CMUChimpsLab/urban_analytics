6de4320d2d639994221e4a4c39e55d06e3752f0a
Dan Tasse <dan.tasse@gmail.com>
2014-01-27 16:42:44 -0500

    fix paging in GetFlickrPhotos

--- GetFlickrPhotos.py
+++ GetFlickrPhotos.py
@@ -14,14 +14,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--start_date',
                    help='latest date to search from. format YYYY-mm-dd',
                    default='2013-08-05')
-# for some reason the flickr api isn't giving me any photos taken after 9/1/13.
+# for some reason, flickr api isn't giving me any photos taken after 2013-8-5.
 # so let's start there and go backwards.
 args = parser.parse_args()
 searchDate = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
 
 db = pymongo.Connection('localhost',27017)['flickr']
 
-PER_PAGE = 500
+# Flickr API says you can go up to 500 photos at a time, but it appears to not
+# actually honor this parameter if you set it to 500? ugh. 250 seems to work.
+PER_PAGE = 250
 
 mainParams = {'method':'flickr.photos.search',\
     'api_key': api_key,\
@@ -50,7 +52,6 @@ def get_these_photos(ids):
         photoInfo = rInfo.json()
         photoInfo['_id'] = photo['id'] # so Mongo uses it as primary ID
         db.flickr_pgh.insert(dict(photoInfo))
-        # print photo['id']
 
 while True:
     mainParams['page'] = 1
@@ -64,13 +65,18 @@ while True:
     id_list = [photo['id'] for photo in r.json()['photos']['photo']]
 
     # catch extra pages if there are any
-    num_pages = num_photos / PER_PAGE + 1
+    num_pages = num_photos / PER_PAGE + 2 # PER_PAGE + 1 should work, but we
+    # give it a little fudge factor because sometimes the count is off. worst
+    # case, you do another request for a page number too high, and it returns
+    # nothing.
+
     for page_num in range(2, num_pages + 1): # +1 because range is exclusive
         mainParams['page'] = page_num
-        r = requests.get('http://api.flickr.com/services/rest/', params=mainParams)
-        id_list += [photo['id'] for photo in r.json()['photos']['photo']]
+        r2 = requests.get('http://api.flickr.com/services/rest/', params=mainParams)
+        id_list += [photo['id'] for photo in r2.json()['photos']['photo']]
         print "Searched for another page, got this many photos now: %d" % len(id_list)
 
+    id_list = list(set(id_list)) # remove duplicates due to paging weirdness
     get_these_photos(id_list)
 
     searchDate -= datetime.timedelta(1)

86497a5842da84424ee2ac0abb1503399c0556e1
Dan Tasse <dan.tasse@gmail.com>
2014-01-29 14:16:06 -0500

    wrap the whole block in a try-except, so one error doesn't kill the whole program

--- GetFlickrPhotos.py
+++ GetFlickrPhotos.py
@@ -56,43 +56,48 @@ def get_these_photos(ids):
         time.sleep(1) # I guess avoid rate limiting? still don't know what the limit is.
 
 while True:
-    mainParams['page'] = 1
-    mainParams['min_taken_date'] = str(searchDate)
-    mainParams['max_taken_date'] = str(searchDate + datetime.timedelta(1)) # 1 day
-    # ... well, as of Jan 28, these parameters seem to be ignored, and it just
-    # returns all photos ever taken in this bbox. Well, okay.
+    try:
+        mainParams['page'] = 1
+        mainParams['min_taken_date'] = str(searchDate)
+        mainParams['max_taken_date'] = str(searchDate + datetime.timedelta(1)) # 1 day
+        # ... well, as of Jan 28, these parameters seem to be ignored, and it just
+        # returns all photos ever taken in this bbox. Well, okay.
 
-    r = requests.get('http://api.flickr.com/services/rest/', params=mainParams)
-    num_photos = int(r.json()['photos']['total'])
-    print "Searched: %s, found this many photos: %s" % (mainParams['min_taken_date'], num_photos)
+        r = requests.get('http://api.flickr.com/services/rest/', params=mainParams)
+        num_photos = int(r.json()['photos']['total'])
+        print "Searched: %s, found this many photos: %s" % (mainParams['min_taken_date'], num_photos)
 
-    id_list = [photo['id'] for photo in r.json()['photos']['photo']]
+        id_list = [photo['id'] for photo in r.json()['photos']['photo']]
 
-    # catch extra pages if there are any
-    num_pages = num_photos / PER_PAGE + 2 # PER_PAGE + 1 should work, but we
-    # give it a little fudge factor because sometimes the count is off. worst
-    # case, you do another request for a page number too high, and it returns
-    # nothing.
+        # catch extra pages if there are any
+        num_pages = num_photos / PER_PAGE + 2 # PER_PAGE + 1 should work, but we
+        # give it a little fudge factor because sometimes the count is off. worst
+        # case, you do another request for a page number too high, and it returns
+        # nothing.
 
-    # get the first page's worth
-    get_these_photos(id_list)
+        # get the first page's worth
+        get_these_photos(id_list)
 
-    # now get all the other pages if there are any
-    # (no real reason to split this up besides to save a request; first page
-    # contains the total count as well as the first 250 results)
-    for page_num in range(2, num_pages + 1): # +1 because range is exclusive
-        mainParams['page'] = page_num
-        r2 = requests.get('http://api.flickr.com/services/rest/', params=mainParams)
-        id_list2 = [photo['id'] for photo in r2.json()['photos']['photo']]
-        get_these_photos(id_list2)
-        print "Searched for page %d, got this many more photos: %d" % (page_num, len(id_list2))
-        
+        # now get all the other pages if there are any
+        # (no real reason to split this up besides to save a request; first page
+        # contains the total count as well as the first 250 results)
+        for page_num in range(2, num_pages + 1): # +1 because range is exclusive
+            mainParams['page'] = page_num
+            r2 = requests.get('http://api.flickr.com/services/rest/', params=mainParams)
+            id_list2 = [photo['id'] for photo in r2.json()['photos']['photo']]
+            get_these_photos(id_list2)
+            print "Searched for page %d, got this many more photos: %d" % (page_num, len(id_list2))
+            
 
-    searchDate -= datetime.timedelta(1)
-    time.sleep(120)
-    # no idea what the rate limit is, and 1 every 4 min ~ 1 year per day
-    # so no need to get greedy.
-    # (note that every photo requires an API call too, so this could make a
-    # lot of calls)
-
+        searchDate -= datetime.timedelta(1)
+        time.sleep(120)
+        # no idea what the rate limit is, and 1 every 4 min ~ 1 year per day
+        # so no need to get greedy.
+        # (note that every photo requires an API call too, so this could make a
+        # lot of calls)
+    except e:
+        print e
+        time.sleep(120)
+        # don't want one error to mess up the whole scraper, or to hammer
+        # flickr and get us rate-limited
 

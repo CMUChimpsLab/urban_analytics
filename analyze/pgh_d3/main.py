acc17a3b3d80ed09e84cea619f0d53562d6f28f3
Dan Tasse <dan.tasse@gmail.com>
2014-03-05 16:23:47 -0500

    okay can finally query for one thing from mongo

--- analyze/pgh_d3/main.py
+++ analyze/pgh_d3/main.py
@@ -3,7 +3,6 @@
 # Server side for plotting things around Pittsburgh.
 
 import flask, pymongo, json, urllib
-from flask import request
 
 app = flask.Flask(__name__)
 dbclient = pymongo.MongoClient('localhost', 27017)
@@ -16,15 +15,13 @@ def main():
 
 @app.route('/query')
 def query():
-    urlquery = request.args.get('query', '')
-    querydecoded = urllib.unquote(urlquery).decode('utf8')
-    queryjson = json.loads(querydecoded)
-    print queryjson
-
-    cursor = db.tweet_pgh.find(queryjson)
+    # request.args.keys()[0] is a string representing the whole query
+    cursor = db.tweet_pgh.find(json.loads(flask.request.args.keys()[0]))
     result1 = cursor.next()
-    return result1
+    del result1['_id'] # because it's an ObjectId; not serializable to json
+
+    return flask.json.jsonify(result1)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True) # TODO remove this from anything deployed
 

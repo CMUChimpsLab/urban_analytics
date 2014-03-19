bc70fcbf538428213397cbc057b56dc91b2171b3
Dan Tasse <dan.tasse@gmail.com>
2014-03-19 19:21:37 -0400

    you can select bounding box points now

--- analyze/pgh_d3/static/main.js
+++ analyze/pgh_d3/static/main.js
@@ -5,6 +5,42 @@ var endDate;
 var allTweets;
 var tweetsToShow;
 
+var width = 1024,
+    height = 768;
+
+var svg = d3.select("#mainSvg")
+    .attr("width", width)
+    .attr("height", height);
+
+var projection = d3.geo.mercator()
+    .center([-79.98, 40.431667]) //-80, 40.441667 is the center of pgh
+    .scale(230000) // ad hoc hacky, works
+    .translate([width/2, height/2]);
+
+// points for the query box, in [lon, lat] form
+var bboxTopLeft = [];
+var bboxBottomRight = [];
+
+// transforms a ... GeometryCollection?... object into a svg path 'd' string
+var path = d3.geo.path().projection(projection);
+// transforms a tweet object into a svg path 'd' string
+var tweetsPath = function(tweet) {
+    // some tweets don't have coordinates, just a tweet.place.bounding_box
+    // (which can be huge, like 7 degrees lat or lon, so forget it)
+    // Note that there is still useful info in tweet.place (like "Pittsburgh")
+    // Anyway I think path(null) just returns null, which works ok
+    return path(tweet.coordinates);
+}
+
+function loadNeighborhoods() {
+    d3.json("static/neighborhoodstopo.json", function(error, nghds) {
+        var neighborhoods = topojson.feature(nghds, nghds.objects.neighborhoods);
+        svg.append("path")
+            .datum(neighborhoods)
+            .attr("d", path);
+    });
+}
+
 // Given an array of tweets, return the ones that are between the values in
 // startDate and endDate
 function filterDates(tweets, startDate, endDate) {
@@ -16,7 +52,7 @@ function filterDates(tweets, startDate, endDate) {
         tweetsToShow = tweetsToShow.filter(function(e) {return new Date(e.created_at) <= endDate;});
     }
     return tweetsToShow;
- }
+}
 
 function update() {
     params = $.parseJSON( $("#query").val() ); // TODO sanitize, obv
@@ -54,38 +90,16 @@ function update() {
     );
 }
 
-var width = 1024,
-    height = 768;
-
-var svg = d3.select("#mainSvg")
-    .attr("width", width)
-    .attr("height", height);
-
-var projection = d3.geo.mercator()
-    .center([-79.98, 40.431667]) //-80, 40.441667 is the center of pgh
-    .scale(230000) // ad hoc hacky, works
-    .translate([width/2, height/2]);
-
-// transforms a ... GeometryCollection?... object into a svg path 'd' string
-var path = d3.geo.path().projection(projection);
-// transforms a tweet object into a svg path 'd' string
-var tweetsPath = function(tweet) {
-    // some tweets don't have coordinates, just a tweet.place.bounding_box
-    // (which can be huge, like 7 degrees lat or lon, so forget it)
-    // Note that there is still useful info in tweet.place (like "Pittsburgh")
-    // Anyway I think path(null) just returns null, which works ok
-    return path(tweet.coordinates);
+function storeBoundingBoxPoint(x, y) {
+    var geoPoint = projection.invert([x, y]);
+    if (bboxTopLeft.length == 0) {
+        bboxTopLeft = geoPoint;
+    } else if (bboxBottomRight.length == 0) {
+        bboxBottomRight = geoPoint;
+    } else { // top left and bottom right both already exist, start new ones
+        bboxTopLeft = geoPoint;
+        bboxBottomRight = [];
+    }
+    $("#topLeftCoords").text(bboxTopLeft);
+    $("#bottomRightCoords").text(bboxBottomRight);
 }
-
-function loadNeighborhoods() {
-    d3.json("static/neighborhoodstopo.json", function(error, nghds) {
-        var neighborhoods = topojson.feature(nghds, nghds.objects.neighborhoods);
-        svg.append("path")
-            .datum(neighborhoods)
-            .attr("d", path);
-    });
-}
-
-// once to start it off
-// update();
-

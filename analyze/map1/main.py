e9d9d9286c5df6291942f1998578b0c2acc20392
Dan Tasse <dan.tasse@gmail.com>
2014-04-18 13:36:09 -0400

    new directory to try out stuff

---
+++ analyze/map1/main.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+
+# Server side for plotting things around Pittsburgh.
+
+import flask, pymongo, json, urllib, random, datetime
+
+app = flask.Flask(__name__)
+dbclient = pymongo.MongoClient('localhost', 27017)
+# or: dbclient = pymongo.MongoClient('mongodb://localhost:27017')
+db = dbclient['tweet']
+
+@app.route('/')
+def main():
+    return flask.render_template('index.html')
+
+@app.route('/query')
+def query():
+    # request.args.keys()[0] is a string representing the whole query
+    cursor = db['tweet_pgh_good'].find(json.loads(flask.request.args.keys()[0]))
+    cursor.limit(2000) # TODO update this limit based on user input
+    results = list(cursor)
+
+    for result in results:
+        del result['_id'] # because it's an ObjectId; not serializable to json
+
+    # has to be a dict, not array, "for security reasons", whatever that means
+    return flask.json.jsonify({'results': results})
+
+@app.route('/bunch_of_tweets')
+def get_a_bunch_of_tweets():
+    limit = int(flask.request.args['limit'])
+    cursor = db['tweet_pgh_good'].find().limit(limit)
+    good_keys = ['text', 'id', 'user', 'coordinates', 'created_at']
+    good_user_keys = ['id', 'screen_name']
+    
+    tweets_to_return = []
+    for tweet in cursor:
+        small_tweet = dict([(key, tweet[key]) for key in good_keys])
+        small_tweet['user'] = dict([(key, tweet['user'][key]) for key in good_user_keys])
+        tweets_to_return.append(small_tweet)
+
+    # has to be a dict, not array, "for security reasons", whatever that means
+    return flask.json.jsonify({'results': tweets_to_return})
+
+
+
+@app.route('/user_centroid_query')
+def user_centroid_query():
+    args = flask.request.args
+    collection_name = args['collection']
+    tl_lon = float(args['top_left_lon'])
+    tl_lat = float(args['top_left_lat'])
+    br_lon = float(args['bottom_right_lon'])
+    br_lat = float(args['bottom_right_lat'])
+    start_time = datetime.time(int(args['start_hour']))
+    end_time = datetime.time(int(args['end_hour']))
+    if args['limit']:
+        limit = int(args['limit'])
+    else:
+        limit = 0
+    if args['per_user_limit']:
+        per_user_limit = int(args['per_user_limit'])
+    else:
+        per_user_limit = 0
+ 
+    tweets_to_return = []
+
+    search_rect = {'type': 'Polygon', 'coordinates': [[
+        [tl_lon, tl_lat],
+        [tl_lon, br_lat],
+        [br_lon, br_lat],
+        [br_lon, tl_lat],
+        [tl_lon, tl_lat]]]}
+    # Note this is a list of lists of coordinates. (you might have multiple
+    # lists of coordinates; like if you have inner rings like a donut.)
+    # Note also that the first and last coordinate are the same.
+
+    cursor = db['user'].find(
+        {'centroid':
+            {'$geoWithin':
+                {'$geometry': search_rect}
+            }
+        })
+    for user in cursor:
+        that_users_tweets = db[collection_name].find({'user.id': user['_id']})
+        that_user_tweet_counter = 0
+        for tweet in that_users_tweets:
+            tweet_time = datetime.datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y').time()
+            if tweet_time < start_time or tweet_time > end_time:
+                continue
+            del tweet['_id'] # b/c it's not serializable
+            tweets_to_return.append(tweet)
+            that_user_tweet_counter += 1
+            if per_user_limit and that_user_tweet_counter >= per_user_limit:
+                break
+        if limit and len(tweets_to_return) >= limit:
+            break
+        
+    return flask.json.jsonify({'results':tweets_to_return})
+
+@app.route('/user_here_once_query')
+def user_here_once_query():
+    args = flask.request.args
+    collection_name = args['collection']
+    tl_lon = float(args['top_left_lon'])
+    tl_lat = float(args['top_left_lat'])
+    br_lon = float(args['bottom_right_lon'])
+    br_lat = float(args['bottom_right_lat'])
+    start_time = datetime.time(int(args['start_hour']))
+    end_time = datetime.time(int(args['end_hour']))
+    if args['limit']:
+        limit = int(args['limit'])
+    else:
+        limit = 0
+    if args['per_user_limit']:
+        per_user_limit = int(args['per_user_limit'])
+    else:
+        per_user_limit = 0
+    # TODO: refactor some of this boilerplate between this and user_centroid_query    
+    tweets_to_return = []
+    search_rect = {'type': 'Polygon', 'coordinates': [[
+        [tl_lon, tl_lat],
+        [tl_lon, br_lat],
+        [br_lon, br_lat],
+        [br_lon, tl_lat],
+        [tl_lon, tl_lat]]]}
+    tweets_in_rect = db[collection_name].find(
+        {'coordinates':
+            {'$geoWithin':
+                {'$geometry': search_rect}
+            }
+        })
+
+    seen_user_ids = [] # don't use the same user ID twice
+    for tweet_in_rect in tweets_in_rect:
+        user_id = tweet_in_rect['user']['id']
+        if user_id in seen_user_ids:
+            continue
+        
+        that_users_tweets = db[collection_name].find({'user.id': user_id})
+        that_user_tweet_counter = 0
+        for tweet in that_users_tweets:
+            tweet_time = datetime.datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y').time()
+            if tweet_time < start_time or tweet_time > end_time:
+                continue
+
+            del tweet['_id'] # not serializable
+
+            # Only send back the essential part of the tweet. Reduces data sent
+            # by about a factor of 10. Have to update this, though, if you want
+            # to use more parts of the tweet.
+            small_tweet = dict([(key, tweet[key]) for key in ['text', 'id','user','coordinates','created_at']])
+            small_tweet['user'] = dict([(key, tweet['user'][key]) for key in ['id', 'screen_name']])
+            tweets_to_return.append(small_tweet)
+            that_user_tweet_counter += 1
+            if per_user_limit and that_user_tweet_counter >= per_user_limit:
+                seen_user_ids.append(user_id)
+                break
+            
+        if limit and len(tweets_to_return) >= limit:
+            break
+
+    return flask.json.jsonify({'results':tweets_to_return})
+
+
+if __name__ == "__main__":
+    print "ensuring indexes"
+    db['user'].ensure_index('_id')
+    db['user'].ensure_index([('centroid', pymongo.GEOSPHERE)])
+    db['tweet_pgh_good'].ensure_index([('coordinates', pymongo.GEOSPHERE)])
+    db['foursquare'].ensure_index([('coordinates', pymongo.GEOSPHERE)])
+    print "indexes have all been created, starting app"
+    app.run(host='0.0.0.0')
+    # 0.0.0.0 means "listen on all public IPs"
+
---
+++ analyze/map1/static/main.js
@@ -0,0 +1,220 @@
+
+// uses backbone, underscore, d3, topojson
+
+var PublicModule = (function() {
+    "use strict";
+    var startDate;
+    var endDate;
+
+    var allTweets;
+    var tweetsToShow;
+    var width=1024, height=768;
+
+    var svg = d3.select("#mainSvg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var projection = d3.geo.mercator()
+        .center([-79.98, 40.431667]) //-80, 40.441667 is the center of pgh
+        .scale(230000) // ad hoc hacky, works
+        .translate([width/2, height/2]);
+
+    // points for the query box, in [lon, lat] form
+    var bboxTopLeft = [];
+    var bboxBottomRight = [];
+
+    // transforms a ... GeometryCollection?... object into a svg path 'd' string
+    var path = d3.geo.path().projection(projection);
+    // transforms a tweet object into a svg path 'd' string
+    var tweetsPath = function(tweet) {
+        // some tweets don't have coordinates, just a tweet.place.bounding_box
+        // (which can be huge, like 7 degrees lat or lon, so forget it)
+        // Note that there is still useful info in tweet.place (like "Pittsburgh")
+        // Anyway I think path(null) just returns null, which works ok
+        return path(tweet.coordinates);
+    }
+    var params;
+
+    // Given an array of tweets, return the ones that are between the values in
+    // startDate and endDate
+    var filterDates = function(tweets, startDate, endDate) {
+        var tweetsToShow = tweets;
+        if (startDate != null) {
+            tweetsToShow = tweetsToShow.filter(function(e) {return new Date(e.created_at) >= startDate;});
+        }
+        if (endDate != null) {
+            tweetsToShow = tweetsToShow.filter(function(e) {return new Date(e.created_at) <= endDate;});
+        }
+        return tweetsToShow;
+    };
+
+    // Returns a color, given a tweet. All tweets from the same user should have
+    // the same color.
+    var generateTweetColor = function(tweet) {
+        var id = tweet.user.id;
+        var hex = (id & 0xffffff).toString(16);
+        return '#' + hex;
+    };
+
+    var update = function() {
+       
+        var tweetSelection = svg.selectAll(".tweet")
+            .data(tweetsToShow)
+            .style("fill", generateTweetColor)
+            .attr("d", tweetsPath);
+
+        tweetSelection.enter().append("path") // .enter() means "if there's more data than dom elements, do this for each new one"
+            .attr("d", tweetsPath)
+            .attr("class", "tweet")
+            .style("fill", generateTweetColor)
+            .on("click", function(tweet) {
+                console.log(tweet.id + "\t " + tweet.user.screen_name + ": " + tweet.text);
+            });
+        tweetSelection.exit().remove();
+
+        $("#loading").hide()
+    };
+
+    var Module = {
+
+        loadNeighborhoods: function() {
+            d3.json("static/neighborhoodstopo.json", function(error, nghds) {
+                var neighborhoods = topojson.feature(nghds, nghds.objects.neighborhoods);
+                svg.append("path")
+                    .datum(neighborhoods)
+                    .attr("d", path)
+                    .attr("class", "neighborhood");
+            });
+        },
+
+        runQuery: function() {
+            params = $.parseJSON( $("#query").val() ); // TODO sanitize, obv
+            // takes about a second per 1k tweets. crashes on 100k.
+            $.getJSON("/query", JSON.stringify(params), function(tweets) {
+                allTweets = tweets.results;
+                update();
+            });
+            $("#loading").show();
+        },
+
+        selectTweetsToShow: function() {
+            var startDate = $("#startDate").datepicker("getDate");
+            var endDate = $("#endDate").datepicker("getDate");
+            var startHour = $("#startTime").val();
+            var endHour = $("#endTime").val();
+
+            var goodTweets = _.filter(allTweets, function(tweet) {
+                var tweetDate = new Date(tweet['created_at']);
+                if (startDate != null && tweetDate < startDate) {
+                    return false;
+                }
+                if (endDate != null && tweetDate > endDate) {
+                    return false;
+                }
+                var tweetHour = tweetDate.getHours();
+                if (tweetHour >= endHour || tweetHour < startHour) {
+                    return false;
+                }
+                return true;
+            });
+            tweetsToShow = _.sample(goodTweets, $("#display_limit").val());
+            update();
+        },
+
+        storeBoundingBoxPoint: function(x, y) {
+            var geoPoint = projection.invert([x, y]);
+            if (bboxTopLeft.length == 0) {
+                bboxTopLeft = geoPoint;
+                
+            } else if (bboxBottomRight.length == 0) {
+                bboxBottomRight = geoPoint;
+            } else { // top left and bottom right both already exist, start new ones
+                bboxTopLeft = geoPoint;
+                bboxBottomRight = [];
+            }
+            $("#topLeftCoords").text(bboxTopLeft[0].toFixed(4) + ", " + bboxTopLeft[1].toFixed(4));
+
+            if (bboxBottomRight.length > 0) {
+                $("#bottomRightCoords").text(bboxBottomRight[0].toFixed(4) + ", " + bboxBottomRight[1].toFixed(4));
+                var width = x - d3.select("#select_box").attr("x");
+                var height = y - d3.select("#select_box").attr("y"); 
+                d3.select("#select_box").attr("width", width).attr("height", height);
+            } else {
+                d3.select("#select_box").attr("x", x).attr("y", y).attr("width", 0).attr("height", 0);
+                $("#bottomRightCoords").text("");
+            }
+        },
+
+        // returns the string name of the collection to query from
+        getCollection: function() {
+            if ($("#useTweets").prop("checked")) {
+                return "tweet_pgh_good";
+            } else if ($("#useFoursquare").prop("checked")) {
+                return "foursquare";
+            } else {
+                alert("error: no collection selected");
+            }
+        },
+
+        // Gets all tweets from all users whose tweet-centroids are within the box
+        // you've drawn.
+        userCentroidQuery: function() {
+            params = {"top_left_lon": bboxTopLeft[0], "top_left_lat":bboxTopLeft[1],
+                "bottom_right_lon": bboxBottomRight[0], "bottom_right_lat": bboxBottomRight[1],
+                "start_hour": $("#startTime").val(),
+                "end_hour": $("#endTime").val(),
+                "limit": $("#server_limit").val(),
+                "per_user_limit": $("#per_user_limit").val(),
+                "collection": getCollection()};
+            $.getJSON("/user_centroid_query", params, function(tweets) {
+                allTweets = tweets.results;
+                tweetsToShow = filterDates(allTweets,
+                    $("#startDate").datepicker("getDate"),
+                    $("#endDate").datepicker("getDate"));
+                update();
+            });
+            $("#loading").show();
+
+        },
+
+        // Gets all tweets from any users who have ever tweeted in the box you've drawn.
+        userHereOnceQuery: function() {
+            params = {"top_left_lon": bboxTopLeft[0], "top_left_lat":bboxTopLeft[1],
+                "bottom_right_lon": bboxBottomRight[0], "bottom_right_lat": bboxBottomRight[1],
+                "start_hour": $("#startTime").val(),
+                "end_hour": $("#endTime").val(),
+                "limit": $("#server_limit").val(),
+                "per_user_limit": $("#per_user_limit").val(),
+                "collection": PublicModule.getCollection()};
+            $.getJSON("/user_here_once_query", params, function(tweets) {
+                allTweets = tweets.results;
+                tweetsToShow = filterDates(allTweets,
+                    $("#startDate").datepicker("getDate"),
+                    $("#endDate").datepicker("getDate"));
+                update();
+            });
+
+            $("#loading").show();
+        },
+
+
+        // Just get a bunch of tweets from the server. (You won't show them all at
+        // the same time.)
+        getBunchOfTweets: function() {
+            params = {
+                "limit": $("#server_limit").val()
+            }
+            $.getJSON("/bunch_of_tweets", params, function(tweets) {
+                allTweets = tweets.results;
+                tweetsToShow = _.sample(allTweets, $("#display_limit").val());
+                update();
+            });
+
+            $("#loading").show();
+        },
+
+        update: update, // make this publicly accessible
+    };
+    return Module;
+}());
+

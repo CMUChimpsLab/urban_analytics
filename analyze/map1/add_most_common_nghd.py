364a2f973c6f968d29ee62e9a0950a3d8d3a4849
Dan Tasse <dan.tasse@gmail.com>
2014-10-17 20:54:20 +0000

    speed up most common nghd script by checking outside pgh first, and checking recent nghds.

--- analyze/map1/add_most_common_nghd.py
+++ analyze/map1/add_most_common_nghd.py
@@ -11,19 +11,27 @@ import cProfile
 
 dbclient = pymongo.MongoClient('localhost', 27017)
 db = dbclient['tweet']
+pittsburgh_outline = None
 
 def load_nghds():
     neighborhoods = geojson.load(open('static/neighborhoods.json'))
     nghds = neighborhoods['features']
     for nghd in nghds:
         nghd['shape'] = shapely.geometry.asShape(nghd.geometry)
+    global pittsburgh_outline
+    pittsburgh_outline = nghds[0]['shape']
+    for nghd in nghds:
+        pittsburgh_outline = pittsburgh_outline.union(nghd['shape'])
     return nghds
     # return [shapely.geometry.asShape(nghd) for nghd in neighborhoods['features']]
 
 # this takes a lot of runtime.
 def get_neighborhood_name(nghds, lon, lat):
     point = shapely.geometry.Point(lon, lat)
-    # TODO add check for "is it in pgh at all?"
+    global pittsburgh_outline # TODO ugh globals
+    if not pittsburgh_outline.contains(point):
+        return 'Outside Pittsburgh'
+    
     for nghd in nghds:
         if nghd['shape'].contains(point):
             # Move this nghd to the front of the queue so it's checked first next time
@@ -45,7 +53,7 @@ def doAll():
         print tweets.count()
         user_nghds = collections.Counter()
         for tweet in tweets:
-            print [n.properties['HOOD'] for n in nghds[0:5]]
+            # print [n.properties['HOOD'] for n in nghds[0:5]]
             user_nghd_name = get_neighborhood_name(nghds,
                 tweet['coordinates']['coordinates'][0],
                 tweet['coordinates']['coordinates'][1])

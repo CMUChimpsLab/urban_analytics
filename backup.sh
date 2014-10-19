b0323ea90657ac53245781b9f0aa6008324d32fc
Hong Bin Shim <hongbins@andrew.cmu.edu>
2014-10-19 19:35:31 +0000

    fixed tar command

--- backup.sh
+++ backup.sh
@@ -2,6 +2,6 @@
 
 echo $(date)
 name=$(date | sed 's/\ /_/g')
-mongodump --out=/data/dbbackup/$name && tar -cvf $name.tar /data/dbbackup/$name && rm -r /data/dbbackup/$name
+mongodump --out=/data/dbbackup/$name && tar -cvf /data/dbbackup/$name.tar /data/dbbackup/$name && rm -r /data/dbbackup/$name
 find /data/dbbackup/ -mtime +3 -exec rm -r {} \;
 echo $(date)

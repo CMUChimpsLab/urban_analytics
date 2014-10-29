413d8ea0ee4ba8503bb720366dffcfc197c82b99
Dan Tasse <dan.tasse@gmail.com>
2014-10-29 15:32:37 -0400

    fix up tweet at common tweeters

--- analyze/home_work/tweet_at_common_tweeters.py
+++ analyze/home_work/tweet_at_common_tweeters.py
@@ -3,7 +3,7 @@
 # Sends tweets to people to invite them to join our study!
 # Reads login info from config.txt.
 
-import argparse, csv, ConfigParser
+import argparse, csv, ConfigParser, pprint, urllib, time
 import oauth2 as oauth
 
 config = ConfigParser.ConfigParser()
@@ -14,15 +14,29 @@ OAUTH_KEYS = {'consumer_key': config.get('twitter', 'consumer_key'),
               'access_token_key': config.get('twitter', 'access_token_key'),
               'access_token_secret': config.get('twitter', 'access_token_secret')}
 
-# TODO
-# POST_PARAMS = {}
-
 # Given a screen name (without @), sends a recruitment tweet to that user, if
-# |actually_send| is true. Otherwise prints to stdout.
+# |actually_send| is True. Otherwise prints to stdout.
+# Returns True iff actually sent a tweet, false otherwise.
 def send_recruitment_tweet(screen_name, actually_send):
-    #if actually_send
-    params = {'status': '@dantasse hello test'}
-    # TODO call oauth_req
+    params = {
+        'status': '@' + screen_name + ' Hi! Saw you tweeted in Pgh. Would you mind taking a voluntary 5min CMU survey? Must be 18+. https://docs.google.com/forms/d/1MtRdolCodDVPP9IWYKDmAUYRCHq2VL_Es_T7xglEP7A/viewform',
+    }
+    post_body=urllib.urlencode(params)
+
+    if not actually_send:
+        print "Would send: " + post_body
+        return False
+
+    (resp, content) = oauth_req(POST_URL, http_method="POST", post_body=post_body)
+    
+    print "Sent: " + post_body
+    if resp['status'] == '200' and 'errors' not in content:
+        return True
+    else:
+        print "Request Failed"
+        pprint.pprint(resp)
+        pprint.pprint(content)
+        return False
 
 # Sends an actual request to Twitter, with authentication.
 # Note! It sends an actual request to Twitter!
@@ -30,12 +44,11 @@ def oauth_req(url, http_method="GET", post_body=None, http_headers=None):
     consumer = oauth.Consumer(key=OAUTH_KEYS['consumer_key'], secret=OAUTH_KEYS['consumer_secret'])
     token = oauth.Token(key=OAUTH_KEYS['access_token_key'], secret=OAUTH_KEYS['access_token_secret'])
     client = oauth.Client(consumer, token)
-    # resp, content = client.request( url, method=http_method, body=post_body, headers=http_headers )
-    resp, content = client.request( url, method=http_method, headers=http_headers )
-    return content
-
-    # print "sending tweet to " + screen_name
-    # print "actually_send: " + str(actually_send)
+    if http_method == "POST":
+        resp, content = client.request(url, method=http_method, body=post_body, headers=http_headers)
+    else:
+        resp, content = client.request(url, method=http_method, headers=http_headers)
+    return (resp, content)
 
 # Given a path to a file containing some common tweeters, returns a dict of
 # user -> num_tweets.
@@ -44,11 +57,6 @@ def get_tweeters(tweeters_filename):
     for row in csv.reader(open(tweeters_filename)):
         retval[row[1]] = int(row[2])
     return retval
-
-# Given a path to a file containing some twitter ids that we've already tried
-# to recruit (one per line), returns a list of those names.
-# def get_already_sent_tweeters(already_tweeted_filename):
-    # return [row[0] for row in csv.reader(open(already_tweeted_filename)) if len(row) > 0]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -70,14 +78,15 @@ if __name__ == '__main__':
     # get a list of the top N tweeters
     tweeters = sorted(tweeters, key = tweeters.get, reverse=True)[0: args.number_to_recruit]
 
-    already_tweeted_file = open(args.already_tweeted_filename, 'a')
+    already_tweeted_file = open(args.already_tweeted_filename, 'a') # for writing
 
-    # home_timeline = oauth_req( 'https://api.twitter.com/1.1/statuses/home_timeline.json' )
-    # print home_timeline
-
+    print "Going to send to these tweeters: " + str(tweeters)
+    time.sleep(10)
     for tweeter in tweeters:
-        send_recruitment_tweet(tweeter, args.really_tweet)
-        already_tweeted_file.write(tweeter + '\n')
+        tweet_worked = send_recruitment_tweet(tweeter, args.really_tweet)
+        if tweet_worked:
+            already_tweeted_file.write(tweeter + '\n')
+        time.sleep(60)
 
     already_tweeted_file.close()
 

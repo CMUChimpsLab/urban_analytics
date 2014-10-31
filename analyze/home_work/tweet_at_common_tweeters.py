1948095ed640206b09c0542d87b2961865aa8015
Dan Tasse <dan.tasse@gmail.com>
2014-10-31 14:45:25 -0400

    add delay and does not exist file parameters

--- analyze/home_work/tweet_at_common_tweeters.py
+++ analyze/home_work/tweet_at_common_tweeters.py
@@ -71,16 +71,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--tweeters_filename', '-f', default='common_tweeters.csv')
     parser.add_argument('--already_tweeted_filename', '-a', default='already_tweeted.csv')
+    parser.add_argument('--does_not_exist_filename', default='does_not_exist.csv')
+    parser.add_argument('--delay', '-d', type=int, default=60, help='Number of seconds between tweets')
     parser.add_argument('--really_tweet', '-r', action='store_true', help='Whether to actually tweet at people, or just print to stdout for debugging.')
     parser.add_argument('--number_to_recruit', '-n', type=int, required=True)
     args = parser.parse_args()
 
     tweeters = get_tweeters(args.tweeters_filename)
 
-    # Remove anyone who's in the already_tweeted file.
+    # Remove anyone who's in the already_tweeted or does_not_exist file.
     already_sent = [row[0] for row in csv.reader(open(args.already_tweeted_filename)) if row]
     print "Already sent to: " + str(already_sent)
     for tweeter in already_sent:
+        if tweeter in tweeters:
+            del tweeters[tweeter]
+    does_not_exist = [row[0] for row in csv.reader(open(args.does_not_exist_filename)) if row]
+    print "Doesn't exist: " + str(does_not_exist)
+    for tweeter in does_not_exist:
         if tweeter in tweeters:
             del tweeters[tweeter]
 
@@ -89,17 +96,19 @@ if __name__ == '__main__':
     tweeters = sorted(tweeters, key = tweeters.get, reverse=True)[0: args.number_to_recruit]
 
     already_tweeted_file = open(args.already_tweeted_filename, 'a') # for writing
+    does_not_exist_file = open(args.does_not_exist_filename, 'a')
 
     print "Going to send to these tweeters: " + str(tweeters)
-    time.sleep(10)
+    time.sleep(5)
     for tweeter in tweeters:
         if user_exists(tweeter):
             tweet_worked = send_recruitment_tweet(tweeter, args.really_tweet)
             if tweet_worked:
                 already_tweeted_file.write(tweeter + '\n')
         else:
+            does_not_exist_file.write(tweeter + '\n')
             print "User doesn't exist anymore: " + tweeter
-        time.sleep(60)
+        time.sleep(args.delay)
 
     already_tweeted_file.close()
 

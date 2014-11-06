7dc995a17d7bd64df7e4cea8365cfb6365783a99
Dan Tasse <dan.tasse@gmail.com>
2014-11-06 16:20:08 -0500

    now filter_non_pgh_tweets removes all non pgh tweets

--- analyze/pgh_d3/filter_non_pgh_tweets.py
+++ analyze/pgh_d3/filter_non_pgh_tweets.py
@@ -15,30 +15,37 @@ dbclient = pymongo.MongoClient('localhost', 27017)
 db = dbclient['tweet']
 
 
-def filter_non_pgh_tweets():
+def remove_non_pgh_tweets():
     counter = 0
     for tweet in db.tweet_pgh.find():
-	counter += 1
-	if (counter % 1000 == 0):
-		print counter
-        if tweet['coordinates'] is not None:
-            
+        counter += 1
+        if (counter % 1000 == 0):
+            print counter
+        if tweet['coordinates'] == None:
+            print "delete, no coordinates"
+            db.tweet_pgh.remove({'_id': tweet['_id']})
+        else:
             lon = tweet['coordinates']['coordinates'][0]
             lat = tweet['coordinates']['coordinates'][1]
             if lon < MIN_LON or lon > MAX_LON:
-		pass
+                print "delete, bad longitude: " + str(lon)
+                db.tweet_pgh.remove({'_id': tweet['_id']})
             elif lat < MIN_LAT or lat > MAX_LAT:
-                pass
+                print "delete, bad latitude: " + str(lat)
+                db.tweet_pgh.remove({'_id': tweet['_id']})
             else:
-                # this is an upsert: inserts if it's not already there
-                db.tweet_pgh_good.update({'_id':tweet['_id']}, tweet, upsert=True)
-        else:
-            pass
+                print "good: " + str(lon) + ", " + str(lat)
 
 if __name__ == '__main__':
-    print "removing all tweets not actually in pittsburgh"
-    filter_non_pgh_tweets()
-    print "ensuring indexes"
-    db['tweet_pgh_good'].ensure_index([('coordinates', pymongo.GEOSPHERE)])
+    print "About to remove all tweets not actually in Pittsburgh"
+    print "that is, all tweets outside Lon %f to %f, and Lat %f to %f" % (MIN_LON, MAX_LON, MIN_LAT, MAX_LAT)
+    print "Are you sure? ctrl-c if not."
+    raw_input()
+    print "count before: " + str(db.tweet_pgh.count())
+    remove_non_pgh_tweets()
+    
+    print "count after: " + str(db.tweet_pgh.count())
+    # print "ensuring indexes"
+    # db['tweet_pgh_good'].ensure_index([('coordinates', pymongo.GEOSPHERE)])
     print "done"
 

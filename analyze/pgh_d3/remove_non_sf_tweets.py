6635cf8a505048a21e0501c81a4b5fa762baa447
Dan Tasse <dan.tasse@gmail.com>
2014-11-07 16:42:54 +0000

    update remove_non_pgh_tweets after running it

--- analyze/pgh_d3/remove_non_sf_tweets.py
+++ analyze/pgh_d3/remove_non_sf_tweets.py
@@ -3,15 +3,15 @@
 # This script has been run already, and should not be necessary to run again.
 # Our Twitter_stream.py now only saves "good" tweets.
 
-# Anyway, what this script *did*: delete any tweet in the tweet_pgh collection
-# that is not actually in Pittsburgh. (sometimes these would be there because
+# Anyway, what this script *did*: delete any tweet in the tweet_sf collection
+# that is not actually in San Francisco. (sometimes these would be there because
 # the twitter API would give us stuff where the actual location is uncertain,
-# but there's a bounding box that includes Pittsburgh.)
+# but there's a bounding box that includes SF.)
 
-MIN_LON = -80.2
-MAX_LON = -79.8
-MIN_LAT = 40.241667
-MAX_LAT = 40.641667
+MIN_LON = -122.595
+MAX_LON = -122.295
+MIN_LAT = 37.565
+MAX_LAT = 37.865
 
 import pymongo
 
@@ -19,37 +19,34 @@ dbclient = pymongo.MongoClient('localhost', 27017)
 db = dbclient['tweet']
 
 
-def remove_non_pgh_tweets():
+def remove_non_sf_tweets():
     counter = 0
-    for tweet in db.tweet_pgh.find():
+    for tweet in db.tweet_sf.find():
         counter += 1
         if (counter % 1000 == 0):
             print counter
         if tweet['coordinates'] == None:
             print "delete, no coordinates"
-            db.tweet_pgh.remove({'_id': tweet['_id']})
+            db.tweet_sf.remove({'_id': tweet['_id']})
         else:
             lon = tweet['coordinates']['coordinates'][0]
             lat = tweet['coordinates']['coordinates'][1]
             if lon < MIN_LON or lon > MAX_LON:
                 print "delete, bad longitude: " + str(lon)
-                db.tweet_pgh.remove({'_id': tweet['_id']})
+                db.tweet_sf.remove({'_id': tweet['_id']})
             elif lat < MIN_LAT or lat > MAX_LAT:
                 print "delete, bad latitude: " + str(lat)
-                db.tweet_pgh.remove({'_id': tweet['_id']})
+                db.tweet_sf.remove({'_id': tweet['_id']})
             else:
                 print "good: " + str(lon) + ", " + str(lat)
 
 if __name__ == '__main__':
-    print "About to remove all tweets not actually in Pittsburgh"
+    print "About to remove all tweets not actually in San Francisco"
     print "that is, all tweets outside Lon %f to %f, and Lat %f to %f" % (MIN_LON, MAX_LON, MIN_LAT, MAX_LAT)
     print "Are you sure? ctrl-c if not."
     raw_input()
-    print "count before: " + str(db.tweet_pgh.count())
-    remove_non_pgh_tweets()
-    
-    print "count after: " + str(db.tweet_pgh.count())
-    # print "ensuring indexes"
-    # db['tweet_pgh_good'].ensure_index([('coordinates', pymongo.GEOSPHERE)])
+    print "count before: " + str(db.tweet_sf.count())
+    remove_non_sf_tweets()
+    print "count after: " + str(db.tweet_sf.count())
     print "done"
 

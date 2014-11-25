0cebe1691938ee4fc821ad689a717d13a7d49beb
Dan Tasse <dan.tasse@gmail.com>
2014-11-25 11:08:27 -0500

    comment out some map4 stuff I'm not using

--- analyze/map4/static/js/app/main.js
+++ analyze/map4/static/js/app/main.js
@@ -6,16 +6,16 @@ define(['jquery', 'app/TweetMap'], function ($, TweetMap) {
         /**
          * Check if user allows geolocation, and center the map if so.
          */
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                tweetMap.setCenter(position);
-            }, function (msg) {
-                console.log(typeof msg == 'string' ? msg : "error");
-            });
-        }
-        else {
-            alert("Geolocation is not supported or allowed on your browser.");
-        }
+        // if(navigator.geolocation) {
+        //     navigator.geolocation.getCurrentPosition(function (position) {
+        //         tweetMap.setCenter(position);
+        //     }, function (msg) {
+        //         console.log(typeof msg == 'string' ? msg : "error");
+        //     });
+        // }
+        // else {
+        //     alert("Geolocation is not supported or allowed on your browser.");
+        // }
 
         // ajax setup
         $.ajaxSetup({
@@ -44,20 +44,20 @@ define(['jquery', 'app/TweetMap'], function ($, TweetMap) {
             });
         });
 
-        $("#get-all-tweets-from-area-btn").on("click", function () {
-            $.ajax({
-                type: "get",
-                data: tweetMap.getSelectedArea(),
-                url: $SCRIPT_ROOT + "/get-all-tweets-from-area",
-                success: function (response) {
-                    tweetMap.clearMap();
-                    tweetMap.plotTweets(response["tweets"]);
-                },
-                error: function () {
-                    console.log("ajax request failed for " + this.url);
-                }
-            });
-        });
+        // $("#get-all-tweets-from-area-btn").on("click", function () {
+        //     $.ajax({
+        //         type: "get",
+        //         data: tweetMap.getSelectedArea(),
+        //         url: $SCRIPT_ROOT + "/get-all-tweets-from-area",
+        //         success: function (response) {
+        //             tweetMap.clearMap();
+        //             tweetMap.plotTweets(response["tweets"]);
+        //         },
+        //         error: function () {
+        //             console.log("ajax request failed for " + this.url);
+        //         }
+        //     });
+        // });
 
         $("#get-user-tweets-btn").on("click", function () {
             $.ajax({
@@ -76,19 +76,19 @@ define(['jquery', 'app/TweetMap'], function ($, TweetMap) {
             });
         });
 
-        $("#get-user-tweets-from-area-btn").on("click", function () {
-            $.ajax({
-                type: "get",
-                data: tweetMap.getSelectedArea(),
-                url: $SCRIPT_ROOT + "/get-user-tweets-from-area",
-                success: function (response) {
-                    tweetMap.clearMap();
-                    tweetMap.plotUsers(response["users"]);
-                },
-                error: function () {
-                    console.log("ajax request failed for " + this.url);
-                }
-            });
-        });
+        // $("#get-user-tweets-from-area-btn").on("click", function () {
+        //     $.ajax({
+        //         type: "get",
+        //         data: tweetMap.getSelectedArea(),
+        //         url: $SCRIPT_ROOT + "/get-user-tweets-from-area",
+        //         success: function (response) {
+        //             tweetMap.clearMap();
+        //             tweetMap.plotUsers(response["users"]);
+        //         },
+        //         error: function () {
+        //             console.log("ajax request failed for " + this.url);
+        //         }
+        //     });
+        // });
     });
 });

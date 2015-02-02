cd6e704cb5ee12f8e18b95990587cd1fc7ebcc60
Dan Tasse <dan.tasse@gmail.com>
2015-02-02 15:54:48 -0500

    ok now load into postgres script works

--- get_data/load_into_postgres.py
+++ get_data/load_into_postgres.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 
 # Pull tweets out of a collection in mongodb and put them into postgresql.
+# Warning! This doesn't check if the tweets are already in there or not.
+# So it may create duplicates.
 
-import argparse, pymongo
+import argparse, pymongo, psycopg2
 
 mongo_db = pymongo.MongoClient('localhost', 27017)['tweet']
+psql_conn = psycopg2.connect("dbname='tweet' user='dtasse' host='localhost'")
+
+cur = psql_conn.cursor()
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--collection', default='tweet_pgh')
     args = parser.parse_args()
 
+    counter = 0
     for tweet in mongo_db['tweet_pgh'].find():
-        print tweet
+        text = tweet['text']
+        user_screen_name = tweet['user']['screen_name']
+        lat = tweet['coordinates']['coordinates'][1]
+        lon = tweet['coordinates']['coordinates'][0]
+        created_at = tweet['created_at']
+
+        cur.execute("""INSERT INTO tweet_pgh(text, user_screen_name, coordinates, created_at)
+            VALUES (%s, %s, Point(%s, %s), %s)  """, \
+            (text, user_screen_name, lat, lon, created_at))
+        psql_conn.commit()
+        counter += 1
+        if counter % 1000 == 0:
+            print str(counter) + " tweets entered"
+
+    psql_conn.close()

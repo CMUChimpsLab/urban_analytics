cb3bd76570f3d01bfa04c0475668841570889b80
Dan Tasse <dan.tasse@gmail.com>
2015-03-10 23:29:15 -0400

    load into postgres works a little bit

--- get_data/load_into_postgres.py
+++ get_data/load_into_postgres.py
@@ -6,25 +6,77 @@
 # So it may create duplicates.
 
 import argparse, pymongo, psycopg2, psycopg2.extensions, ppygis, traceback
+import pytz, datetime
 
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 
 mongo_db = pymongo.MongoClient('localhost', 27017)['tweet']
-psql_conn = psycopg2.connect("dbname='tweet' user='dantasse'")
+psql_conn = psycopg2.connect("dbname='tweet'")
 
 pg_cur = psql_conn.cursor()
 
-def tweet_to_db_row(tweet):
-    text = tweet['text']
-    user_screen_name = tweet['user']['screen_name']
+month_map = {'Jan': 1, 'Feb': 2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 
+    'Aug':8,  'Sep': 9, 'Oct':10, 'Nov': 11, 'Dec': 12}
+# Special case date parsing. All our dates are like this:
+# Wed Jan 22 23:19:19 +0000 2014
+# 012345678901234567890123456789
+# so let's just parse them like that. 
+def parse_date(d):
+    return datetime.datetime(int(d[26:30]), month_map[d[4:7]], int(d[8:10]),\
+        int(d[11:13]), int(d[14:16]), int(d[17:19]), 0, pytz.timezone('UTC')) # 0=usec
+
+# Map of field name -> postgres datatype. Contains everything we want to save.
+# TODO: if you change this, also change the tweet_to_insert_string method below.
+data_types = {
+    # _id skipped; id (no underscore) works well enough for us.
+    # 'contributors': 'ARRAY', # TODO does this work?
+    'coordinates': 'Point',
+    'created_at': 'timestamp',
+    # 'entities': 'hstore', # TODO this is tricky too.
+    'favorite_count': 'integer',
+    # |favorited| only makes sense if there's an authenticated user.
+    'filter_level': 'text',
+    # geo skipped, b/c it's deprecated
+    'lang': 'text',
+    'id': 'bigint primary key',
+    # id_str skipped; same as ID.
+    'in_reply_to_screen_name': 'text',
+    'in_reply_to_status_id': 'bigint',
+    'in_reply_to_status_id_str': 'text',
+    'in_reply_to_user_id': 'bigint',
+    'in_reply_to_user_id_str': 'text',
+    # 'place': 'hstore', # TODO this is tricky.
+    'retweet_count': 'integer',
+    # |retweeted| only make sense if there's an authenticated user.
+    'source': 'text',
+    'text': 'text NOT NULL',
+    # truncated is obsolete; Twitter now rejects long tweets instead of truncating.
+    # 'user': 'hstore', # TODO this may be tricky.
+    'user_screen_name': 'text NOT NULL', # added this
+}
+    
+# Argument: a tweet JSON object. Returns: a string starting with "INSERT..."
+# that you can run to insert this tweet into a Postgres database.
+def tweet_to_insert_string(tweet):
     lat = tweet['coordinates']['coordinates'][1]
     lon = tweet['coordinates']['coordinates'][0]
-    created_at = tweet['created_at']
-    insert_str = cur.mogrify("INSERT INTO tweet_pgh(text, user_screen_name, " +
-            "coordinates, created_at)" + 
-            "VALUES (%s, %s, Point(%s, %s), %s)  ", \
-            (text, user_screen_name, lat, lon, created_at))
+    coordinates = ppygis.Point(lon, lat, srid=4326)
+    created_at = parse_date(tweet['created_at'])
+
+    insert_str = pg_cur.mogrify("INSERT INTO tweet_pgh(" +
+            "coordinates, created_at, favorite_count, filter_level, " +
+            "lang, id, in_reply_to_screen_name, in_reply_to_status_id, " +
+            "in_reply_to_status_id_str, in_reply_to_user_id, in_reply_to_user_id_str, " +
+            "retweet_count, source, text, user_screen_name) " + 
+            "VALUES (" + ','.join(['%s' for key in data_types]) + ")", 
+        (coordinates, created_at, tweet['favorite_count'], 
+        tweet['filter_level'], tweet['lang'], tweet['id'],
+        tweet['in_reply_to_screen_name'], tweet['in_reply_to_status_id'],
+        tweet['in_reply_to_status_id_str'], tweet['in_reply_to_user_id'],
+        tweet['in_reply_to_user_id_str'], tweet['retweet_count'],
+        tweet['source'], tweet['text'],
+        tweet['user']['screen_name']))
     return insert_str
 
     
@@ -34,30 +86,30 @@ if __name__=='__main__':
     args = parser.parse_args()
 
     print "About to copy Mongo to Postgres. Enter to continue, Ctrl-C to quit."
+    raw_input()
     pg_cur.execute("DROP TABLE IF EXISTS tweet_pgh;")
-    pg_cur.execute("""CREATE TABLE tweet_pgh(
-        id integer primary key,
-        favorite_count integer,
-        lang string,
-        created_at string,
-        retweeted bool, 
-        user_screen_name string NOT NULL,
-        text string NOT NULL
-        );""")
-    cur.execute("SELECT AddGeometryColumn('tweet_pgh', 'coordinates', 4326, 'POINT', 2)")
+    psql_conn.commit()
+    create_table_str = "CREATE TABLE tweet_pgh("
+    for key, value in sorted(data_types.iteritems()):
+        if key not in ['coordinates']: # create that coords column separately.
+            create_table_str += key + ' ' + value + ', '
+    create_table_str = create_table_str[:-2] + ");"
+
+    pg_cur.execute(create_table_str)
+    psql_conn.commit()
+    pg_cur.execute("SELECT AddGeometryColumn('tweet_pgh', 'coordinates', 4326, 'POINT', 2)")
+    psql_conn.commit()
 
     counter = 0
     for tweet in mongo_db['tweet_pgh'].find():
-        insert_str = tweet_to_db_row(tweet)
-        print insert_str
+        insert_str = tweet_to_insert_string(tweet)
         try:
-            cur.execute(insert_str)
+            pg_cur.execute(insert_str)
             psql_conn.commit()
         except Exception as e:
-            print "Error on tweet from user " + user_screen_name
-            print "tweet name: %s, text: %s, lon=%d, lat=%d, created_at=%s" % (user_screen_name, text, lon, lat, created_at)
-            # traceback.print_exc()
-            # traceback.print_stack()
+            print "Error running this command: %s" % insert_str
+            traceback.print_exc()
+            traceback.print_stack()
             psql_conn.commit()
 
         counter += 1

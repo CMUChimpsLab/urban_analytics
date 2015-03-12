52db30dacd767bd07ad8dc9dde571b21da8ac1ce
Dan Tasse <dan.tasse@gmail.com>
2015-03-12 15:41:02 -0400

    update backup script to get postgres db too

--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@
 
 echo $(date)
 name=$(date | sed 's/\ /_/g')
+pg_dumpall > /data/dbbackup/pg_$name && tar -cvf /data/dbbackup/pg_$name.tar /data/dbbackup/pg_$name && rm -r /data/dbbackup/pg_$name
 mongodump --out=/data/dbbackup/$name && tar -cvf /data/dbbackup/$name.tar /data/dbbackup/$name && rm -r /data/dbbackup/$name
 echo "removing..."
 find /data/dbbackup/ -mtime 3 -exec echo {} \;

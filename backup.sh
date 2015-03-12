36103be3d0e343337fc6aa3e3cc6e4d5512cc598
Dan Tasse <dan.tasse@gmail.com>
2015-03-12 15:44:26 -0400

    uh make the backup a sql file

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 echo $(date)
 name=$(date | sed 's/\ /_/g')
-pg_dumpall > /data/dbbackup/pg_$name && tar -cvf /data/dbbackup/pg_$name.tar /data/dbbackup/pg_$name && rm -r /data/dbbackup/pg_$name
+pg_dumpall > /data/dbbackup/pg_$name.sql && tar -cvf /data/dbbackup/pg_$name.tar /data/dbbackup/pg_$name.sql && rm -r /data/dbbackup/pg_$name.sql
 mongodump --out=/data/dbbackup/$name && tar -cvf /data/dbbackup/$name.tar /data/dbbackup/$name && rm -r /data/dbbackup/$name
 echo "removing..."
 find /data/dbbackup/ -mtime 3 -exec echo {} \;
